c72cabc4354550102900fb0c7c042b3feba6f72e
sahhar dhia eddine <dhiachabane@yhaoo.fr>
2022-12-12 19:21:59 +0100

    deployment edit11

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,6 +1,16 @@
 /* eslint-disable-line */
 const AboutUsPage = () => {
-  return <div>hello</div>;
+  return (
+    <div>
+      <h1> About The Project</h1>
+      <p>This is a simple demonstration prject not meant for production .</p>
+      <h3>About the dev </h3>
+      <p>
+        Made by Sahhar Dhia Eddine a fullstack dev dont hesitate to contact me
+      </p>
+      <a href="https://portfolio-tau-five-71.vercel.app/">Protfolio</a>
+    </div>
+  );
 };
 
 export default AboutUsPage;
