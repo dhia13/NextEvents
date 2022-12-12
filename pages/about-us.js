834b8dcfb41715e1b486bec182ef88009030a803
sahhar dhia eddine <dhiachabane@yhaoo.fr>
2022-12-12 18:34:50 +0100

    events website

---
+++ pages/about-us.js
@@ -0,0 +1,53 @@
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1> About The Project</h1>
+      <p>This is a simple demonstration prject not meant for production .</p>
+      <h2>Dlor in reprehenderit </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <ul className="about_us_list">
+        <li>
+          <h3>Dlor in reprehenderit </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in
+          </p>
+        </li>
+        <li>
+          <h3>Sed do eiusmod tempor </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in
+          </p>
+        </li>{" "}
+        <li>
+          <h3>About the dev </h3>
+          <p>
+            Made by Sahhar Dhia Eddine a fullstack dev don't hesitate to contact
+            me
+            <br />
+            <a href="https://portfolio-tau-five-71.vercel.app/">
+              <h4>Protfolio</h4>
+            </a>
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AboutUsPage;
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <p> © 2022 Kizaru Dev - A Project Built with Next.js </p>
+    </footer>
+  );
+};
